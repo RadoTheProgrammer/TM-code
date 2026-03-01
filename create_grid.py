d44c5df2dfd282047955f1493adc7c07673c1181
RadoTheProgrammer <rrrado15@gmail.com>
2026-03-01 19:53:31 +0100

    test avec nouvelles données

--- create_grid.py
+++ create_grid.py
@@ -1,14 +1,20 @@
+"""
+Crée la matrice d envie
+"""
+print("hello")
 import pandas as pd
 
-df = pd.read_csv("input.csv",index_col=0)
-gen_col = [0]*8
-data = {}
-for i,eleve in df.iterrows():
-    col = gen_col.copy()
-    col[eleve["Choice1"]] = 3
-    col[eleve["Choice2"]] = 2
-    col[eleve["Choice3"]] = 1
-    data[i] = col
+df = pd.read_excel("Donnees_TMs/Année 1/TM année 1_voeux élèves.xlsx",index_col=0)
 
-df_grid = pd.DataFrame(data)
+df_grid = pd.DataFrame(0,index=df.index,columns=range(1,36))
+gen_col = [0]*35
+data = {}
+for nom_eleve,eleve in df.iterrows():
+
+    for nchoix in (1,2,3):
+        choix = eleve[f"Choix {nchoix}"]
+        if choix==0:
+            continue
+        df_grid.at[nom_eleve,int(choix[2:])] = 4-nchoix
+
 df_grid.to_csv("grid.csv")
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-INPUT_FILE = "input.csv"
+INPUT_FILE = "grid.csv"
 OUTPUT_FILE = "output.csv"
-TM_FILE = "tm.csv"
+TM_FILE = "Donnees_TMs/Année 1/Liste sujets TM + jauge_année 1.xlsx"
 
 
 
@@ -8,22 +8,26 @@ import random
 import pandas as pd
 
 
-df_grid = pd.read_csv("grid.csv",index_col=0).transpose()
+df_grid_orig = pd.read_csv(INPUT_FILE,index_col=0)
 
-df_tm = pd.read_csv(TM_FILE,index_col=0)
-decision_data = {"Id":[], "Choice":[], "ChoiceWeight":[]}
+df_tm = pd.read_excel(TM_FILE,index_col=0)
+df_tm = df_tm.iloc[:-1] # enlever TM libre
+
 N_TRIES = 1000
 def generate():
     # Génère une répartition aléatoire, erreur si ça échoue
+    df_grid = df_grid_orig.copy()
+    decision_data = {"Id": [], "Choice": [], "ChoiceWeight": []}
     for i_tm,tm in df_tm.sample(frac=1).iterrows():
         #print(i_tm)
-        mask = df_grid[i_tm] > 0
+        mask = df_grid[str(i_tm)] > 0
 
         result = df_grid[mask]
-
+        result.choic
         l = len(result)
         #if l<tm["Nmin"]
-        n = random.randrange(tm["Nmax"]+1)
+        #n = random.randrange(int(tm["Nombre maximal travaux"])+1)
+        n = int(tm["Nombre maximal travaux"])
         if n<l:
             selected = result.sample(n)
         else:
@@ -34,21 +38,31 @@ def generate():
         for i_eleve,eleve in result.iterrows():
 
             if i_eleve in selected.index:
+                choice_weight = eleve[str(i_tm)]
                 decision_data["Id"].append(i_eleve)
                 decision_data["Choice"].append(i_tm)
-                decision_data["ChoiceWeight"].append(eleve[i_tm])
-
+                decision_data["ChoiceWeight"].append(choice_weight)
+                #df_grid.loc[i_eleve] = 0
+                #df_grid.at[i_eleve,i_tm] = choice_weight
             else:
-                df_grid.loc[i_eleve,i_tm] = 0
+                df_grid.at[i_eleve,str(i_tm)] = 0
+                pass
             pass
         df_grid.drop(selected.index,inplace=True)
         pass
-    print(df_grid)
-    return len(df_grid)
+    l1,l2 = len(df_grid), len(decision_data["Choice"])
+    #print(l1,l2,l1+l2)
+    df_decision_data = pd.DataFrame(decision_data)
+    df_decision_data.sort_values(by=["Id"],inplace=True)
+    return l2
     if len(df_grid)>0:
         return False # échec
     return True
     pass
 generate()
+max_l2 = 0
 for i_try in range(N_TRIES):
-    print(i_try,generate())
+    l2 = generate()
+    print(l2)
+    if l2>max_l2:
+        max_l2 = l2
