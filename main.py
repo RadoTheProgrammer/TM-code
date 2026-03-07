cc048360aca3675c9e50d60bf6cf33ebf3c5f815
RadoTheProgrammer <rrrado15@gmail.com>
2026-03-07 16:59:07 +0100

    comparison sum vs none vs mean, 3 data.csv

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 INPUT_FILE = "grid.csv"
-OUTPUT_DIR = "results2"
-OUTPUT_FILE = f"{OUTPUT_DIR}/results.csv"
+OUTPUT_DIR = "results2-none"
+OUTPUT_FILE = f"{OUTPUT_DIR}/data.csv"
 TM_FILE = "Donnees_TMs/Année 1/Liste sujets TM + jauge_année 1.xlsx"
-N_TRIES = 1000
-
+N_TRIES = 256
+MODE = "none" # "sum" ou "mean" ou "none"
 
 import os
 import random
@@ -17,10 +17,9 @@ df_grid_orig = pd.read_csv(INPUT_FILE,index_col=0)
 df_tm = pd.read_excel(TM_FILE,index_col=0)
 df_tm = df_tm.iloc[:-1] # enlever TM libre
 
-best_mean = 0
-best_std = 0
 
-results = {"Id":[],"Mean":[],"Std":[]}
+
+results = {"Id":[],"Mean":[],"Std":[],"Problems":[]}
 if os.path.exists(OUTPUT_DIR):
     shutil.rmtree(OUTPUT_DIR,ignore_errors=True)
 else:
@@ -30,6 +29,7 @@ def generate():
     # Génère une répartition aléatoire, erreur si ça échoue
     df_grid = df_grid_orig.copy()
     decision_data = {"Id": [], "Choice": [], "ChoiceWeight": []}
+    problems = []
     for i_tm,tm in df_tm.sample(frac=1).iterrows():
         #print(i_tm)
         mask = df_grid[str(i_tm)] > 0
@@ -38,29 +38,45 @@ def generate():
         #result.choic
         l = len(result)
         a = result[str(i_tm)]
-        b = result.drop(columns=[str(i_tm)]).mean(axis=1)
+        result1 = result.drop(columns=[str(i_tm)])
+        if MODE in ("sum","none"):
+            b = result1.sum(axis=1)
+        elif MODE=="mean":
+            b = result1.mean(axis=1)
+
         #print(result.drop(columns=[str(i_tm)]).mean(axis=1))
         #print(result[str(i_tm)])
-        weights = a/b
+        if MODE=="none":
+            weights = pd.Series([1]*len(result), index=result.index)
+        else:
+            weights = a/b
         #if l<tm["Nmin"]
         #n = random.randrange(int(tm["Nombre maximal travaux"])+1)
         
-        forced = result[b==0]
+
         n = int(tm["Nombre maximal travaux"])
 
         if n<l:
-            forced = result[b.isna()]
+            if MODE in ("sum","none"):
+                forced_bool = b==0
+            else:
+                forced_bool = b.isna()
+            forced = result[forced_bool]
             if len(forced):
                 pass
 
-            weights[b.isna()] = 0 
+            weights[forced_bool] = 0 
             #print(b)
             #print(b.isna())# Ceux qui n'ont pas d'autres choix
             n_to_assign = n-len(forced)
-            if n_to_assign>0:
+            if MODE=="none":
+                weights = None
+            if n_to_assign>=0:
+                
                 selected2 = result.sample(n-len(forced),weights=weights)
                 selected = pd.concat([forced,selected2])
-            else:
+            else: # PROBLEM !!!!
+                problems.append((i_tm,n_to_assign))
                 selected = forced
         else:
             selected = result
@@ -86,13 +102,14 @@ def generate():
 
     df_decision_data = pd.DataFrame(decision_data)
     if len(df_grid)==len(df_decision_data): # Succès
-        df_decision_data.to_csv(f"{OUTPUT_DIR}/{i_try}.csv",index=False)
+        df_decision_data.to_csv(f"{OUTPUT_DIR}/r{i_try}.csv",index=False)
         mean = df_decision_data["ChoiceWeight"].mean()
         std = df_decision_data["ChoiceWeight"].std()
         results["Id"].append(i_try)
         results["Mean"].append(mean)
         results["Std"].append(std)
-        print(f"Try {i_try}: mean={mean}, std={std}")
+        results["Problems"].append(problems)
+        print(f"Try {i_try}: mean={mean}, std={std}, problems={problems}")
 
         pass
     
